482607f129e85f86eef956885455240bc39e4b92
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-09 14:16:26 +0000

    add image url to about section to loop over in motion div

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,31 @@
 import React, {useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-
+import { images } from '../../constants';
 import './About.scss';
+
+const abouts = [
+  { 
+    title: 'Front-End',
+    description: 'I am a good web developer.',
+    imgUrl: images.about01,
+  },
+  { 
+    title: 'Back-End',
+    description: 'I am a good web developer.',
+    imgUrl: images.about02,
+  },
+  { 
+    title: 'Full-Stack',
+    description: 'I am a good web developer.',
+    imgUrl: images.about03,
+  },
+  { 
+    title: 'Web Development',
+    description: 'I am a good web developer.',
+    imgUrl: images.about04,
+  },
+]
 
 const About = () => {
   return (
@@ -18,7 +41,19 @@ const About = () => {
       </h2>
 
       <div className='app__profiles'>
-        
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
       </div>
     </>
   )
