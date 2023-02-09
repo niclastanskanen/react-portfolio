4591a0db4229240482ada7786783bf6fcd83283e
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-09 14:08:36 +0000

    add content to about section with basic boilerplate

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 
 import './About.scss';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+        <span>Full-Stack</span>
+        Software Developer
+        <br />
+        with
+        <br />
+        Advanced
+        <span>Front-End</span>
+      </h2>
+
+      <div className='app__profiles'>
+        
+      </div>
+    </>
   )
 }
 
