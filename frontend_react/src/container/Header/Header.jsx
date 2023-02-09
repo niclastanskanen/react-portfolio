46132e8a97b7557cf4d3a3fb6112d165f1609806
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-09 13:10:31 +0000

    add scalevariants in header motion div

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -4,6 +4,17 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <div className='app__header app__flex'>
@@ -32,9 +43,20 @@ const Header = () => {
         className='app__header-img'
       >
         <img src={images.profile} alt='profile_bg' />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt='profile_circle'
+          className='overlay_circle'
+        />
       </motion.div>
 
-      <motion.div>
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
 
       </motion.div>
     </div>
