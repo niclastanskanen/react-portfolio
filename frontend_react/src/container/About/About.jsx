ddb06e38173cdd379028c5fad6fe5535e6ea3b26
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-10 09:56:25 +0000

    fix about container to fetch data from sanity backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,53 +1,41 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  { 
-    title: 'Front-End',
-    description: 'I am a good web developer.',
-    imgUrl: images.about01,
-  },
-  { 
-    title: 'Back-End',
-    description: 'I am a good web developer.',
-    imgUrl: images.about02,
-  },
-  { 
-    title: 'Full-Stack',
-    description: 'I am a good web developer.',
-    imgUrl: images.about03,
-  },
-  { 
-    title: 'Web Development',
-    description: 'I am a good web developer.',
-    imgUrl: images.about04,
-  },
-]
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className='head-text'><span>Full-Stack</span><br/>Software Developer</h2>
-      <div className='app__profiles'>
+
+      <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
-            className='app__profile-item'
+            className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
