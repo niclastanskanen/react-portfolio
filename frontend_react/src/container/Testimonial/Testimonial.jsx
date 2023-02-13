4d9b9a8a53ca0d3d197446bde860c7577683b7a9
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-13 20:16:55 +0000

    add testimonials content with feedback, name and company

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -8,28 +8,46 @@ import './Testimonial.scss';
 
 const Testimonial = () => {
 
-  const [brands, setBrands] = useState([]);
-  const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
     const brandsQuery = '*[_type == "brands"]';
 
-    client.fetch(query)
-      .then((data) => {
-        setTestimonials(data);
-      })
+    client.fetch(query).then((data) => {
+      setTestimonials(data);
+    });
 
-    client.fetch(brandsQuery)
-      .then((data) => {
-        setBrands(data);
-      })
-  }, [])
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
 
+  const test = testimonials[currentIndex];
 
   return (
-    <div>Testimonial</div>
+    <>
+      {testimonials.length && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+          {/* <img src={urlFor(test.imgurl)} alt={test.name} /> */}
+            <div className='app__testimonial-content'>
+              <p className='p-text'>{test.feedback}</p>
+              <div>
+                <h4 className='bold-text'>{test.name}</h4>
+                <h5 className='p-text'>{test.company}</h5>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
