b44a3a1547c9539a430dcc4968c69fa1129b1787
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-13 13:10:35 +0000

    add icons on work with motion

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -51,7 +51,34 @@ const Work = () => {
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <motion.div
+                whileHover={{ opacity: [0,1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className='app__work-hover app__flex'
+              >
+                <a href={work.projectLink} target='_blank' rel='noreferr'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target='_blank' rel='noreferr'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
             </div>
+            <div></div>
           </div>
         ))}
       </motion.div>
