04dcee0518c8cee2734b0a497cb3c36dc41c7a98
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-14 12:50:41 +0000

    add footer content with email and phone number

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -7,7 +7,24 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>take a coffe & chat with me</h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:niclas@tanskanen.me' className='p-text'>niclas@tanskanen.me</a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='mobile' />
+          <a href='tel: +46738179000' className='p-text'>+46738179000</a>
+        </div>
+      </div>
+      <div className='app__footer-form app__flex'>
+        <div className='app__flex'>
+          
+        </div>
+      </div>
+    </>
   )
 }
 
