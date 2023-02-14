3ac12bcde96290f2de1be9f4f2bd926ebf1a291a
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-14 08:39:57 +0000

    add icon arrows with handleclick on testimonials

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -14,7 +14,7 @@ const Testimonial = () => {
 
   const handleClick = (index) => {
     setCurrentIndex(index);
-  };
+  }
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
@@ -43,6 +43,14 @@ const Testimonial = () => {
                 <h4 className='bold-text'>{test.name}</h4>
                 <h5 className='p-text'>{test.company}</h5>
               </div>
+            </div>
+          </div>
+          <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1 )}>
+              <HiChevronLeft />
+            </div>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1 )}>
+              <HiChevronRight />
             </div>
           </div>
         </>
