3fcb394d46c95d389117e4e84d54af7811582bae
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-15 09:18:46 +0000

    remove assets picture that is not in use

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -67,7 +67,7 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target='_blank' rel='noreferr'>
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -77,7 +77,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target='_blank' rel='noreferr'>
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
