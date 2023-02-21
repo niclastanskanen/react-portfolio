aba37aa1996c9313ee9ec435963ba1f58050e561
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-21 15:08:53 +0000

    fix aria label for a href and compressed profile picture

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -7,12 +7,12 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-          <a href='https://www.linkedin.com/in/niclastanskanen/' target='_blank' rel='noreferrer'>
+          <a href='https://www.linkedin.com/in/niclastanskanen/' target='_blank' rel='noreferrer' aria-label='LinkedIn'>
             <BsLinkedin />
           </a>
         </div>
         <div>
-          <a href='https://github.com/niclastanskanen' target='_blank' rel='noreferrer'>
+          <a href='https://github.com/niclastanskanen' target='_blank' rel='noreferrer' aria-label='GitHub'>
             <BsGithub />
           </a>
         </div>
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -67,7 +67,7 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                <a href={work.projectLink} target='_blank' rel='noreferrer' aria-label='Live Site'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -77,7 +77,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                <a href={work.codeLink} target='_blank' rel='noreferrer' aria-label='GitHub'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
